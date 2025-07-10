a75eaedf2a4a21eb9c6f985744c39494f07ba01c
Aleksandar <aleksandar@avasilic.com>
2025-07-10 14:31:20 +0900

    please
    Updated the Go protobuf package path to 'emma/gen/go/proto/v1' in the .proto file and all Go source files. Adjusted CI workflow to check the new generated file location. This change improves versioning and organization of protobuf definitions.

--- internal/handlers/handler.go
+++ internal/handlers/handler.go
@@ -1,10 +1,10 @@
 package handlers
 
 import (
-	"emma/gen/go/proto"
+	"emma/gen/go/proto/v1"
 )
 
 type Handler interface {
-	Fetch(config map[string]interface{}) ([]*proto.DataPoint, error)
+	Fetch(config map[string]interface{}) ([]*v1.DataPoint, error)
 	Validate(config map[string]interface{}) error
 }
